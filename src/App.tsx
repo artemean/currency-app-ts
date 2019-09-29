885e742118ff6783a9835413189a82548c5b3ac0
Andrii Artemenko <andrey.artemenko@gmail.com>
2019-09-29 23:22:40 +0200

    Implement currency selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
 import React, { useState, useEffect } from "react";
 import CurrencyListCom from "./components/CurrencyList";
-import { Currency, CurrencyResponse } from "./types/types";
+import { CurrencyResponse } from "./types/types";
 import "./App.scss";
 
 const App: React.FC = () => {
   const [currencies, setCurrencies] = useState();
-  const getData = async () =>
-    await (await fetch("https://api.exchangeratesapi.io/latest")).json();
+  const [selectedBaseCurrency = "EUR", setSelectedBaseCurrency] = useState();
+  const getData = async (currency: string) => {
+    // console.log(selectedBaseCurrency);
+    return await (await fetch(
+      `https://api.exchangeratesapi.io/latest?base=${currency}`
+    )).json();
+  };
   useEffect(() => {
-    if (!currencies) {
-      getData().then(data => setCurrencies(data));
-    }
-  });
+    getData(selectedBaseCurrency).then(data => setCurrencies(data));
+  }, [selectedBaseCurrency]);
 
+  const onChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
+    setSelectedBaseCurrency(event.target.value);
+  };
   const loading = <div>Loading...</div>;
 
   const renderApp = (currencies: CurrencyResponse) => (
     <div className="currency-app">
       <h1>Currencies App</h1>
-      <h2>Base currency: {currencies.base}</h2>
-      <select name="currencies-list" id="currencies-dropdown">
+      <h2>Base currency: {selectedBaseCurrency}</h2>
+      <select onChange={onChange} name="currency-list" id="currencies-dropdown">
         {Object.keys(currencies.rates).map(currency => (
-          <option>{currency}</option>
+          <option key={currency}>{currency}</option>
         ))}
       </select>
       <h3>For date: {currencies.date}</h3>
       <CurrencyListCom currencies={currencies} />
     </div>
   );
-  console.log(currencies);
+
   return currencies ? renderApp(currencies) : loading;
 };
 
