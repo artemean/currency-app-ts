33406c801b37747a10cb72761e1670c8b46a8a94
Andrii Artemenko <andrey.artemenko@gmail.com>
2019-09-29 20:21:16 +0200

    Add country flags

--- src/components/CurrencyBox.tsx
+++ src/components/CurrencyBox.tsx
@@ -1,12 +1,14 @@
-import React from 'react';
-import {formatDollar} from '../utils/utils';
-import {Currency} from '../types/types';
-import styles from './currency-box.module.scss';
+import React from "react";
+import { formatDollar } from "../utils/utils";
+import { Currency } from "../types/types";
+import styles from "./currency-box.module.scss";
 
-const CurrencyBox: React.FC<Currency> = ({name, value}) => (
-    <div className={styles.wrap}>
-        <span className={styles.name}>{name}</span> - <span className={styles.value}>{formatDollar(value, name)}</span>
-    </div>
+const CurrencyBox: React.FC<Currency> = ({ name, value }) => (
+  <div className={styles.wrap}>
+    <img src={`https://www.countryflags.io/${name.slice(0, 2)}/flat/32.png`} />
+    <span className={styles.name}>{name}</span> -{" "}
+    <span className={styles.value}>{formatDollar(value, name)}</span>
+  </div>
 );
 
 export default CurrencyBox;
